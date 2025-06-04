1f5a03aa7c80c957fefb7e2ca033130470e2def4
Vanesa Lujan Alejandro <vanesalujana@gmail.com>
2025-06-04 22:56:50 +0200

    agregue variables con prompt y condicional if a js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Simulador de agenda de citas//
+
+let nombre = prompt("¿Cual es tu nombre?")
+let edad = prompt("¿Que edad tienes?")
+let diasesion = prompt("¿Que dia quieres agendar tu sesion?")
+let motivodeconsulta = prompt ("¿Cual es tu motivo de consulta?")
+
+
+let sesionconfirmada = true
+
+if (sesionconfirmada) {
+  console.log("Gracias por agendar tu sesión. Nos vemos pronto :)")
+} else {
+  console.log("La sesión aún no fue confirmada. Por favor, completa tus datos para confirmarla.")
+}
+
+
+
+
+
+
+
